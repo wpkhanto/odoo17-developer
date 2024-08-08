ebe10fa48e0a8b1ab3f67aa4dd065398e1de0e2e
wpkhanto <wpkhanto@icloud.com>
2024-08-08 17:55:32 +0700

    add tutorial owl

---
+++ custom_addons/owl/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "OWL Tutorial",
+    'version': '1.0',
+    'summary': 'OWL Tutorial',
+    'sequence': -1,
+    'description': """OWL Tutorial""",
+    'category': 'OWL',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'asset': {
+        'web.assets_backend': [],
+    },
+}
