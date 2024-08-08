018b6232d7b0eb39b9702f8104396b5dcab44be0
wpkhanto <wpkhanto@icloud.com>
2024-08-08 17:54:30 +0700

    add test custom login button

---
+++ custom_addons/my_custom_login_button/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Login Button',
+    'version': '1.0',
+    'category': 'Web',
+    'summary': 'Add custom button to login page',
+    'depends': ['web'],
+    'data': [
+        'views/web_login_inherit.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
