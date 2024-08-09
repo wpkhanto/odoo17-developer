21cb8eb97ea0be26a9e973666c672174149fda29
wpkhanto <wpkhanto@icloud.com>
2024-08-09 17:51:16 +0700

    Start OWL Implementation, "manifest depends add web is important"

--- custom_addons/owl/__manifest__.py
+++ custom_addons/owl/__manifest__.py
@@ -1,20 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "OWL Tutorial",
-    'version': '1.0',
+    'name' : 'OWL Tutorial',
+    'version' : '1.0',
     'summary': 'OWL Tutorial',
     'sequence': -1,
     'description': """OWL Tutorial""",
     'category': 'OWL',
-    'depends': ['base'],
+    'depends' : ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
         'views/todo_list.xml',
     ],
-    'demo': [],
+    'demo': [
+    ],
     'installable': True,
     'application': True,
-    'asset': {
-        'web.assets_backend': [],
+    'assets': {
+        'web.assets_backend': [
+            'owl/static/src/components/*/*.js',
+            'owl/static/src/components/*/*.xml',
+            'owl/static/src/components/*/*.scss',
+        ],
     },
 }
